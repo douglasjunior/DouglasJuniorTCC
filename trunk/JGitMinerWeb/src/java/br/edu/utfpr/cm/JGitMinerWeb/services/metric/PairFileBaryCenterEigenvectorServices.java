17735ee613c24bf3ac30a6b55eed0f6da7accbea
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-03-05 00:04:01 -0300

    Alteração de tradução de matriz para matrix.

--- trunk/JGitMinerWeb/src/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/PairFileBaryCenterEigenvectorServices.java
+++ trunk/JGitMinerWeb/src/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/PairFileBaryCenterEigenvectorServices.java
@@ -5,13 +5,13 @@
 package br.edu.utfpr.cm.JGitMinerWeb.services.metric;
 
 import br.edu.utfpr.cm.JGitMinerWeb.dao.GenericDao;
-import br.edu.utfpr.cm.JGitMinerWeb.model.matriz.EntityMatriz;
-import br.edu.utfpr.cm.JGitMinerWeb.model.matriz.EntityMatrizNode;
+import br.edu.utfpr.cm.JGitMinerWeb.model.matrix.EntityMatrix;
+import br.edu.utfpr.cm.JGitMinerWeb.model.matrix.EntityMatrixNode;
 import br.edu.utfpr.cm.JGitMinerWeb.model.miner.EntityRepository;
-import br.edu.utfpr.cm.JGitMinerWeb.services.matriz.UserCommentedSameFileInDateServices;
-import br.edu.utfpr.cm.JGitMinerWeb.services.matriz.UserModifySameFileInDateServices;
-import br.edu.utfpr.cm.JGitMinerWeb.services.matriz.auxiliary.AuxFileCountSum;
-import br.edu.utfpr.cm.JGitMinerWeb.services.matriz.auxiliary.AuxUserFile;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.UserCommentedSameFileInDateServices;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.UserModifySameFileInDateServices;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.auxiliary.AuxFileCountSum;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.auxiliary.AuxUserFile;
 import br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary.AuxFileMetrics;
 import br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary.AuxUserMetrics;
 import br.edu.utfpr.cm.JGitMinerWeb.util.JsfUtil;
@@ -29,8 +29,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
- *
- * @author Douglas
+ * 
+ * @author Rodrigo T. Kuroda
  */
 public class PairFileBaryCenterEigenvectorServices extends AbstractMetricServices {
 
@@ -40,8 +40,8 @@ public class PairFileBaryCenterEigenvectorServices extends AbstractMetricService
         super(dao, out);
     }
 
-    public PairFileBaryCenterEigenvectorServices(GenericDao dao, EntityMatriz matriz, Map params, OutLog out) {
-        super(dao, matriz, params, out);
+    public PairFileBaryCenterEigenvectorServices(GenericDao dao, EntityMatrix matrix, Map params, OutLog out) {
+        super(dao, matrix, params, out);
     }
 
     public Date getFutureBeginDate() {
@@ -52,47 +52,29 @@ public class PairFileBaryCenterEigenvectorServices extends AbstractMetricService
         return getDateParam("futureEndDate");
     }
 
-    /*
-     *
-     Code churn => The number of lines of code that were either added or changed over the history of this file.
-     Updates => The number of updates to the repository that included this file.
-     Developers => The number of distinct developers who have updated this file over its history.
-     (Sum/Average/Max) of Degree => The (sum/average/maximum) of each developer’s degree over a file’s history.
-     (Sum/Average/Max) of Closeness => The (sum/average/maximum) of each developer’s Closeness over a file’s history.
-     (Sum/Average/Max) of Betweenness => The (sum/average/maximum) of each developer’s Betweenness over a file’s history.
-     Number of Hub Developers => The number of distinct hub developers who update this file.
-     *
-     Código alterado => O número de linhas de código que foram ou adicionados ou alterados ao longo da história deste arquivo.
-     Atualizações => O número de atualizações para o repositório que incluiu este arquivo.
-     Desenvolvedores => O número de desenvolvedores distintos que atualizaram este arquivo longo de sua história.
-     (Soma / Média / Max) de grau => O (soma / média / máxima) de grau de cada desenvolvedor sobre a história de um arquivo.
-     (Soma / Média / Max) de proximidade => O (soma / média / máxima) de proximidade de cada desenvolvedor sobre a história de um arquivo.
-     (Soma / Média / Max) de intermediação => O (soma média / / máximo) de intermediação de cada desenvolvedor sobre a história de um arquivo.
-     Número de Desenvolvedores de Hub => O número de desenvolvedores hub distintas que atualizam esse arquivo.
-     */
     @Override
     public void run() {
         System.out.println(params);
 
-        System.out.println(getMatriz().getClassServicesName());
+        System.out.println(getMatrix().getClassServicesName());
 
-        if (getMatriz() == null
-                || !getAvailableMatricesPermitted().contains(getMatriz().getClassServicesName())) {
-            throw new IllegalArgumentException("Selecione uma matriz gerada pelo Services: " + getAvailableMatricesPermitted());
+        if (getMatrix() == null
+                || !getAvailableMatricesPermitted().contains(getMatrix().getClassServicesName())) {
+            throw new IllegalArgumentException("Selecione uma matrix gerada pelo Services: " + getAvailableMatricesPermitted());
         }
 
         repository = getRepository();
 
         if (repository == null) {
-            throw new IllegalArgumentException("Não foi possível encontrar o repositório utilizado nesta matriz.");
+            throw new IllegalArgumentException("Não foi possível encontrar o repositório utilizado nesta matrix.");
         }
 
-        System.out.println("Selecionado matriz com " + getMatriz().getNodes().size() + " nodes.");
+        System.out.println("Selecionado matrix com " + getMatrix().getNodes().size() + " nodes.");
         UndirectedSparseMultigraph<String, String> graphMulti = new UndirectedSparseMultigraph<>();
         UndirectedSparseGraph<String, String> graph = new UndirectedSparseGraph<>();
         List<String> files = new ArrayList<>();
-        for (int i = 0; i < getMatriz().getNodes().size(); i++) {
-            EntityMatrizNode node = getMatriz().getNodes().get(i);
+        for (int i = 0; i < getMatrix().getNodes().size(); i++) {
+            EntityMatrixNode node = getMatrix().getNodes().get(i);
             String[] coluns = node.getLine().split(JsfUtil.TOKEN_SEPARATOR);
             colectFile(files, coluns[2]);
             graph.addEdge(
@@ -229,7 +211,7 @@ public class PairFileBaryCenterEigenvectorServices extends AbstractMetricService
     }
 
     private EntityRepository getRepository() {
-        String[] repoStr = getMatriz().getRepository().split("/");
+        String[] repoStr = getMatrix().getRepository().split("/");
         List<EntityRepository> repos = dao.executeNamedQueryWithParams(
                 "Repository.findByNameAndOwner",
                 new String[]{"login", "name"},
