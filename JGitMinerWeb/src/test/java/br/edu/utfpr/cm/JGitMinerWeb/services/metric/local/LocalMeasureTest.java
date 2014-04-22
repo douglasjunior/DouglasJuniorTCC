896e187e084c9a949ecafb5825e4a464b659a46d
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-04-21 22:05:24 -0300

    Test for LocalMeasure class

---
+++ JGitMinerWeb/src/test/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/local/LocalMeasureTest.java
@@ -0,0 +1,36 @@
+
+package br.edu.utfpr.cm.JGitMinerWeb.services.metric.local;
+
+import br.edu.utfpr.cm.JGitMinerWeb.services.metric.local.LocalMeasure;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rodrigo T. Kuroda
+ */
+public class LocalMeasureTest {
+    
+    /**
+     * Test of equals method, of class LocalMeasure.
+     */
+    @Test
+    public void testEqualsTrue() {
+        LocalMeasure<String> v1 = new LocalMeasure<>("V", 0, 0, 0);
+        LocalMeasure<String> v2 = new LocalMeasure<>("V", 0, 0, 0);
+        
+        assertTrue(v1.equals(v2));
+    }
+    
+    /**
+     * Test of equals method, of class LocalMeasure.
+     */
+    @Test
+    public void testEqualsFalse() {
+        LocalMeasure<String> v1 = new LocalMeasure<>("V1", 0, 0, 0);
+        LocalMeasure<String> v2 = new LocalMeasure<>("V2", 0, 0, 0);
+        
+        assertFalse(v1.equals(v2));
+    }
+    
+}
