3ad3aad326980172907e220df312c52bfa00b527
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-04-28 23:10:00 -0300

    Finalized implementation of ego network metrics.

--- JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/CochangeEgoNetworkMeasureServices.java
+++ JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/CochangeEgoNetworkMeasureServices.java
@@ -1,23 +1,30 @@
 package br.edu.utfpr.cm.JGitMinerWeb.services.metric;
 
 import br.edu.utfpr.cm.JGitMinerWeb.dao.GenericDao;
+import br.edu.utfpr.cm.JGitMinerWeb.dao.PairFileDAO;
 import br.edu.utfpr.cm.JGitMinerWeb.model.matrix.EntityMatrix;
 import br.edu.utfpr.cm.JGitMinerWeb.model.matrix.EntityMatrixNode;
 import br.edu.utfpr.cm.JGitMinerWeb.model.miner.EntityRepository;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.UserCommentedSamePairOfFileInDateServices;
 import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.UserModifySamePairOfFileInDateServices;
-import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.auxiliary.AuxFileCountSum;
-import br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary.AuxFileMetrics;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.auxiliary.AuxFileFile;
+import br.edu.utfpr.cm.JGitMinerWeb.services.matrix.auxiliary.AuxUserUser;
+import br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary.AuxFileFileMetrics;
+import br.edu.utfpr.cm.JGitMinerWeb.services.metric.ego.EgoMeasure;
+import br.edu.utfpr.cm.JGitMinerWeb.services.metric.ego.EgoMeasureCalculator;
 import br.edu.utfpr.cm.JGitMinerWeb.util.JsfUtil;
 import br.edu.utfpr.cm.JGitMinerWeb.util.OutLog;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
-import edu.uci.ics.jung.graph.UndirectedSparseMultigraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -27,12 +34,56 @@ public class CochangeEgoNetworkMeasureServices extends AbstractMetricServices {
 
     private EntityRepository repository;
 
+    private final PairFileDAO pairFileDAO;
+
     public CochangeEgoNetworkMeasureServices(GenericDao dao, OutLog out) {
         super(dao, out);
+        pairFileDAO = new PairFileDAO(dao);
     }
 
     public CochangeEgoNetworkMeasureServices(GenericDao dao, EntityMatrix matrix, Map params, OutLog out) {
         super(dao, matrix, params, out);
+        pairFileDAO = new PairFileDAO(dao);
+    }
+
+    public Date getBeginDate() {
+        Calendar beginDateCalendar = Calendar.getInstance();
+        beginDateCalendar.setTime(getDateParam("beginDate"));
+        beginDateCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        beginDateCalendar.set(Calendar.MINUTE, 0);
+        beginDateCalendar.set(Calendar.SECOND, 0);
+        beginDateCalendar.set(Calendar.MILLISECOND, 0);
+        return beginDateCalendar.getTime();
+    }
+
+    public Date getEndDate() {
+        Calendar endDateCalendar = Calendar.getInstance();
+        endDateCalendar.setTime(getDateParam("endDate"));
+        endDateCalendar.set(Calendar.HOUR_OF_DAY, 23);
+        endDateCalendar.set(Calendar.MINUTE, 59);
+        endDateCalendar.set(Calendar.SECOND, 59);
+        endDateCalendar.set(Calendar.MILLISECOND, 999);
+        return endDateCalendar.getTime();
+    }
+
+    public Date getFutureBeginDate() {
+        Calendar futureBeginDateCalendar = Calendar.getInstance();
+        futureBeginDateCalendar.setTime(getDateParam("futureBeginDate"));
+        futureBeginDateCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        futureBeginDateCalendar.set(Calendar.MINUTE, 0);
+        futureBeginDateCalendar.set(Calendar.SECOND, 0);
+        futureBeginDateCalendar.set(Calendar.MILLISECOND, 0);
+        return futureBeginDateCalendar.getTime();
+    }
+
+    public Date getFutureEndDate() {
+        Calendar futureEndDateCalendar = Calendar.getInstance();
+        futureEndDateCalendar.setTime(getDateParam("futureEndDate"));
+        futureEndDateCalendar.set(Calendar.HOUR_OF_DAY, 23);
+        futureEndDateCalendar.set(Calendar.MINUTE, 59);
+        futureEndDateCalendar.set(Calendar.SECOND, 59);
+        futureEndDateCalendar.set(Calendar.MILLISECOND, 999);
+        return futureEndDateCalendar.getTime();
     }
 
     @Override
@@ -40,6 +91,11 @@ public class CochangeEgoNetworkMeasureServices extends AbstractMetricServices {
         System.out.println(params);
 
         System.out.println(getMatrix().getClassServicesName());
+
+        Date beginDate = getBeginDate();
+        Date endDate = getEndDate();
+        Date futureBeginDate = getFutureBeginDate();
+        Date futureEndDate = getFutureEndDate();
 
         if (getMatrix() == null
                 || !getAvailableMatricesPermitted().contains(getMatrix().getClassServicesName())) {
@@ -53,126 +109,181 @@ public class CochangeEgoNetworkMeasureServices extends AbstractMetricServices {
         }
 
         System.out.println("Selecionado matrix com " + getMatrix().getNodes().size() + " nodes.");
-        
-        final UndirectedSparseMultigraph<String, String> graphMulti = new UndirectedSparseMultigraph<>();
+
         final UndirectedSparseGraph<String, String> graph = new UndirectedSparseGraph<>();
-        
-        final Map<String, Double> edgeWeigth = new HashMap<>(getMatrix().getNodes().size());
-        List<String> files = new ArrayList<>();
-        List<String> commiters = new ArrayList<>();
+
+        final Map<String, Integer> edgeWeigth = new HashMap<>(getMatrix().getNodes().size());
+        final Map<AuxFileFile, Set<String>> commitersPairFile = new HashMap<>();
+        final Map<AuxFileFile, Long> pairFileCommitCount = new HashMap<>();
+
+        final Set<String> distinctDevelopers = new HashSet<>();
+        final Set<String> distinctFiles = new HashSet<>();
+
+        Set<AuxFileFile> pairFiles = new HashSet<>();
+        Map<String, AuxFileFile> edgeFiles = new HashMap<>();
         for (int i = 0; i < getMatrix().getNodes().size(); i++) {
             EntityMatrixNode node = getMatrix().getNodes().get(i);
             String[] columns = node.getLine().split(JsfUtil.TOKEN_SEPARATOR);
-            colectFile(files, columns[2]);
-            colectCommiter(commiters, columns[0]);
-            edgeWeigth.put(columns[2] + "(" + i + ")", Double.valueOf(columns[4]));
-            graph.addEdge(
-                    columns[2] + "(" + i + ")",
-                    columns[1],
-                    columns[0],
-                    EdgeType.UNDIRECTED);
-            graphMulti.addEdge(
-                    columns[2] + "(" + i + ")",
-                    columns[1],
-                    columns[0],
-                    EdgeType.UNDIRECTED);
+
+            AuxFileFile pairFile = new AuxFileFile(columns[1], columns[2]);
+            AuxUserUser pairUser = new AuxUserUser(columns[0], columns[3]);
+
+            // to count unique developers
+            distinctDevelopers.add(pairUser.getUser());
+            distinctDevelopers.add(pairUser.getUser2());
+
+            /**
+             * Extract all distinct developer that commit a pair of file
+             */
+            if (commitersPairFile.containsKey(pairFile)) {
+                Set<String> commiters = commitersPairFile.get(pairFile);
+                commiters.add(pairUser.getUser());
+                commiters.add(pairUser.getUser2());
+            } else {
+                Set<String> commiters = new HashSet<>();
+                commiters.add(pairUser.getUser());
+                commiters.add(pairUser.getUser2());
+                commitersPairFile.put(pairFile, commiters);
+            }
+
+            String edgeName = pairUser.toString();
+
+            // to count unique files
+            distinctFiles.add(pairFile.getFileName());
+            distinctFiles.add(pairFile.getFileName2());
+
+            pairFiles.add(pairFile);
+            edgeFiles.put(edgeName, pairFile);
+
+            String weightedEdge = (String) params.get("weightedEdge");
+            if ("false".equalsIgnoreCase(weightedEdge)) {
+                // binary edge weight
+                edgeWeigth.put(edgeName, 1);
+            } else {
+                /* Sum commit for each pair file that the pair dev has commited. */
+                if (edgeWeigth.containsKey(pairUser.toStringUserAndUser2())) {
+                    // edgeName = user + user2
+                    edgeWeigth.put(pairUser.toStringUserAndUser2(), edgeWeigth.get(pairUser.toStringUserAndUser2()) + Integer.valueOf(columns[4]));
+                } else if (edgeWeigth.containsKey(pairUser.toStringUser2AndUser())) {
+                    // edgeName = user2 + user
+                    edgeWeigth.put(pairUser.toStringUser2AndUser(), edgeWeigth.get(pairUser.toStringUser2AndUser()) + Integer.valueOf(columns[4]));
+                } else {
+                    edgeWeigth.put(edgeName, Integer.valueOf(columns[4]));
+                }
+            }
+
+            if (!pairFileCommitCount.containsKey(pairFile)) {
+                pairFileCommitCount.put(pairFile,
+                        pairFileDAO.calculeSumUpdatesOfTwoFile( repository,
+                                pairFile.getFileName(), pairFile.getFileName2(), 
+                                beginDate, endDate));
+            }
+
+            /**
+             * The co-change network is constructed as follow: If developer A
+             * and developer B commits a pair file PF, then they are connected
+             * in network.
+             */
+            if (!graph.containsVertex(pairUser.getUser())
+                    || !graph.containsVertex(pairUser.getUser2())
+                    || !graph.isNeighbor(pairUser.getUser(), pairUser.getUser2())) {
+                graph.addEdge(edgeName, pairUser.getUser(), pairUser.getUser2(), EdgeType.UNDIRECTED);
+            }
+        }
+        out.printLog("Weigth of edges:");
+        for (Map.Entry<String, Integer> entry : edgeWeigth.entrySet()) {
+            out.printLog(entry.getKey() + " " + entry.getValue());
+        }
+        out.printLog("End of weigth of edges.");
+
+        /**
+         * Calculates structural holes metrics for each developer on the
+         * co-change network based.
+         */
+        Map<String, EgoMeasure<String>> metricsResult = EgoMeasureCalculator.calcule(graph);
+
+        /**
+         * For each pair of file, calculate the sum, average, and max of each
+         * structural holes metrics of developer that commited the pair of file.
+         */
+        List<AuxFileFileMetrics> egoMetrics = new ArrayList<>(pairFiles.size());
+        for (AuxFileFile pairFile : pairFiles) {
+            long sizeMax = 0, sizeSum = 0;
+            long pairsMax = 0, pairsSum = 0;
+            long tiesMax = 0, tiesSum = 0;
+            double sizeAvg, pairsAvg, tiesAvg;
+            double densityMax = 0, densityAvg, densitySum = 0;
+            double betweennessMax = 0, betweennessAvg, betweennessSum = 0;
+            long developers = 0;
+            for (String commiter : commitersPairFile.get(pairFile)) {
+                EgoMeasure<String> commiterMetric = metricsResult.get(commiter);
+                long size = commiterMetric.getSize();
+                long pairs = commiterMetric.getPairs();
+                long ties = commiterMetric.getTies();
+                double density = commiterMetric.getDensity();
+                double betweenness = commiterMetric.getBetweennessCentrality();
+
+                sizeSum += size;
+                pairsSum += pairs;
+                tiesSum += ties;
+                densitySum += density;
+                betweennessSum += betweenness;
+
+                sizeMax = Math.max(sizeMax, size);
+                pairsMax = Math.max(pairsMax, pairs);
+                tiesMax = Math.max(tiesMax, ties);
+                densityMax = Math.max(densityMax, density);
+                betweennessMax = Math.max(betweennessMax, betweenness);
+
+                developers++;
+            }
+            sizeAvg = (double) sizeSum / (double) developers;
+            tiesAvg = (double) tiesSum / (double) developers;
+            pairsAvg = (double) pairsSum / (double) developers;
+            densityAvg = densitySum / developers;
+            betweennessAvg = betweennessSum / developers;
+
+            Long updates = pairFileDAO.calculeUpdates(repository,
+                            pairFile.getFileName(), pairFile.getFileName2(),
+                            beginDate, endDate);
+            Long futureUpdates = pairFileDAO.calculeUpdates(repository,
+                            pairFile.getFileName(), pairFile.getFileName2(),
+                            futureBeginDate, futureEndDate);
+
+            egoMetrics.add(
+                    new AuxFileFileMetrics(
+                            pairFile.getFileName(), pairFile.getFileName2(),
+                            sizeSum, sizeAvg, sizeMax,
+                            pairsSum, pairsAvg, pairsMax,
+                            tiesSum, tiesAvg, tiesMax,
+                            densitySum, densityAvg, densityMax,
+                            betweennessSum, betweennessAvg, betweennessMax,
+                            developers, pairFileCommitCount.get(pairFile), updates, futureUpdates));
         }
 
-        // in undirected network, these measures are same for all
-        List<AuxFileMetrics> fileMetrics = new ArrayList<>();
-        int size = graphMulti.getVertexCount(); // The number of nodes in the ego network [#16, p.113, #23, p.5]
-        int pairs = size * (size - 1); // Number of possible directed edges in the ego network [#23, p.5]
-        int ties = graphMulti.getEdgeCount(); // Number of edges in the ego network [#16, p.113]
-        int density = pairs == 0 ? 0 : ties / pairs; // Proportion of possible ties that actually are present (Ties/Pairs) [#16, p.113, #23, p.5]
-                
-        for (String commiter : commiters) {
-            int inDegree = graphMulti.inDegree(commiter);
-            int outDegree = graphMulti.outDegree(commiter);
-            int inOutDegree = inDegree + outDegree;
-            int weakComponent = size - graphMulti.getNeighborCount(commiter); // weakly connected (acho que é nós ligados indiretamente) [#16, p.113]
-            int normalizedWeakComponent = size == 0 ? 0 : weakComponent / size;
-            
-            int twoStepReach = graphMulti.getNeighborCount(commiter); // The proportion of nodes that are within two hops of ego [#16, p.113]
-            for (String neighboor : graphMulti.getNeighbors(commiter)) {
-                twoStepReach += graphMulti.getNeighborCount(neighboor);
-            }
-            int reachEfficiency = size == 0 ? 0 : twoStepReach / size;
-            fileMetrics.add(new AuxFileMetrics(commiter,
-                    inDegree, outDegree, inOutDegree, 
-                    density, weakComponent, normalizedWeakComponent, 
-                    twoStepReach, reachEfficiency,
-                    size, pairs, ties));
-            
-        }
-        addToEntityMetricNodeList(fileMetrics);
+        addToEntityMetricNodeList(egoMetrics);
+
+        out.printLog("Distinct developers: " + distinctDevelopers.size());
+        out.printLog("Distinct files: " + distinctFiles.size());
     }
 
     @Override
     public String getHeadCSV() {
-        return "file;"
-                + "inDregree;outDegree;inOutDegree;"
-                + "density;weakComponent;normalizedWeakComponent;"
-                + "twoStepReach;reachEfficiency;"
-                + "size;pairs;ties;"
-                ;
+        return "file;file2;"
+                + "sizeSum;sizeAvg;sizeMax;"
+                + "pairsSum;pairsAvg;pairsMax;"
+                + "tiesSum;tiesAvg;tiesMax;"
+                + "densitySum;densityAvg;densityMax;"
+                + "betweennessSum;betweennessAvg;betweennessMax;"
+                + "developers;commit;updates;futureUpdates";
     }
 
     @Override
     public List<String> getAvailableMatricesPermitted() {
         return Arrays.asList(
-                UserModifySamePairOfFileInDateServices.class.getName()
+                UserModifySamePairOfFileInDateServices.class.getName(),
+                UserCommentedSamePairOfFileInDateServices.class.getName()
         );
-    }
-
-    private void colectFile(List<String> files, String file) {
-        if (!files.contains(file)) {
-            files.add(file);
-        }
-    }
-
-    private void colectCommiter(List<String> commiters, String commiter) {
-        if (!commiters.contains(commiter)) {
-            commiters.add(commiter);
-        }
-    }
-
-    private Double calculeMax(Double v, Double vMax) {
-        if (vMax < v) {
-            return v;
-        }
-        return vMax;
-    }
-
-    private AuxFileCountSum calculeCodeChurnAndUpdates(String fileName, Date beginDate, Date endDate) {
-        String jpql = "SELECT NEW " + AuxFileCountSum.class.getName() + "(f.filename, COUNT(f), SUM(f.changes)) "
-                + "FROM "
-                + "EntityRepositoryCommit rc JOIN rc.files f  "
-                + "WHERE "
-                + "rc.repository = :repo AND "
-                + "rc.commit.committer.dateCommitUser BETWEEN :beginDate AND :endDate AND "
-                + "f.filename = :fileName "
-                + "GROUP BY f.filename";
-
-        String[] bdParams = new String[]{
-            "repo",
-            "fileName",
-            "beginDate",
-            "endDate"
-        };
-        Object[] bdObjects = new Object[]{
-            repository,
-            fileName,
-            beginDate,
-            endDate
-        };
-
-        List<AuxFileCountSum> result = dao.selectWithParams(jpql, bdParams, bdObjects);
-
-        if (!result.isEmpty()) {
-            return result.get(0);
-        } else {
-            return new AuxFileCountSum(fileName, 0, 0);
-        }
     }
 
     private EntityRepository getRepository() {
