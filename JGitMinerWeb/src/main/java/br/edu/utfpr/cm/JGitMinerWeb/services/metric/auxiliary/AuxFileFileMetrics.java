bd8fe8c39898cdfb390fd69cde835ecfbbb9c233
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-11-03 23:52:09 -0200

    Remove unused import and comment

--- JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/auxiliary/AuxFileFileMetrics.java
+++ JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/auxiliary/AuxFileFileMetrics.java
@@ -1,12 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary;
 
 import br.edu.utfpr.cm.JGitMinerWeb.util.Util;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
