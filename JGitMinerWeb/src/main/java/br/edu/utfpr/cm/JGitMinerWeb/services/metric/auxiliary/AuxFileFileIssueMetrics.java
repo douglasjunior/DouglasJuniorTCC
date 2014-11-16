1e94477b404008b2a2652d9ad106d03c5d27cdda
Rodrigo T. Kuroda <rodrigokuroda@gmail.com>
2014-11-15 22:00:40 -0200

    Fix equals.

--- JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/auxiliary/AuxFileFileIssueMetrics.java
+++ JGitMinerWeb/src/main/java/br/edu/utfpr/cm/JGitMinerWeb/services/metric/auxiliary/AuxFileFileIssueMetrics.java
@@ -1,6 +1,8 @@
 package br.edu.utfpr.cm.JGitMinerWeb.services.metric.auxiliary;
 
 import br.edu.utfpr.cm.JGitMinerWeb.util.Util;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,18 +10,38 @@ import java.util.Objects;
  *
  * @author douglas
  */
-public class AuxFileFileIssueMetrics extends AuxFileFileMetrics {
+public class AuxFileFileIssueMetrics {
 
     private final Integer issue;
+    private final String file;
+    private final String file2;
+    private final List<Double> metrics;
 
     public AuxFileFileIssueMetrics(String file, String file2, Integer issue, double... metrics) {
-        super(file, file2, metrics);
+        this.file = file;
+        this.file2 = file2;
+        this.metrics = new ArrayList<>();
         this.issue = issue;
+        addMetrics(metrics);
     }
 
     public AuxFileFileIssueMetrics(String file, String file2, Integer issue, List<Double> metrics) {
-        super(file, file2, metrics);
+        this.file = file;
+        this.file2 = file2;
+        this.metrics = metrics;
         this.issue = issue;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public String getFile2() {
+        return file2;
+    }
+
+    public List<Double> getMetrics() {
+        return Collections.unmodifiableList(metrics);
     }
 
     @Override
@@ -35,7 +57,8 @@ public class AuxFileFileIssueMetrics extends AuxFileFileMetrics {
 
     @Override
     public int hashCode() {
-        int hash = super.hashCode();
+        int hash = 7;
+        hash = 89 * hash + (Objects.hashCode(this.file) + Objects.hashCode(this.file2));
         hash = 89 * hash + Objects.hashCode(this.issue);
         return hash;
     }
@@ -49,10 +72,22 @@ public class AuxFileFileIssueMetrics extends AuxFileFileMetrics {
             return false;
         }
         final AuxFileFileIssueMetrics other = (AuxFileFileIssueMetrics) obj;
-        if (issue != null && this.issue.equals(other.issue)) {
-            return super.equals(obj);
+        if (Objects.equals(issue, other.issue)) {
+            if (Util.stringEquals(this.file, other.file)
+                    && Util.stringEquals(this.file2, other.file2)) {
+                return true;
+            }
+            if (Util.stringEquals(this.file, other.file2)
+                    && Util.stringEquals(this.file2, other.file)) {
+                return true;
+            }
         }
         return false;
     }
 
+    public void addMetrics(double... metrics) {
+        for (double value : metrics) {
+            this.metrics.add(value);
+        }
+    }
 }
